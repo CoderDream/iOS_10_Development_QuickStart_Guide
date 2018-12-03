440b0508c4cacbdf5892a4dac60efb4182e4dea6
许林 <xulin.wh@qq.com>
2018-12-03 10:18:08 +0800

    chapter02 20181203

--- Instagram/Instagram/SignInVC.swift
+++ Instagram/Instagram/SignInVC.swift
@@ -10,18 +10,25 @@ import UIKit
 
 class SignInVC: UITableViewCell {
 
+    // 输入框：用户名、密码
     @IBOutlet weak var usernameTxt: UITextField!
     @IBOutlet weak var passwordTxt: UITextField!
     
-    
+    // 按钮：登录、注册
     @IBOutlet weak var signInBtn: UIButton!
     @IBOutlet weak var signUpBtn: UIButton!
+    
+    // 按钮：忘记密码
+    @IBOutlet weak var forgotBtn: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    @IBAction func signInBtnClicked(_ sender: UIButton) {
+        print("登录按钮被单击")
+    }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
