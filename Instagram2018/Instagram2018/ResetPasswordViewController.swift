f856ea77ee4e234a2fbbb09214bbc0ed478f1e50
许林 <xulin.wh@qq.com>
2018-12-05 10:01:07 +0800

    add 3 ViewController and remove old ViewController

---
+++ Instagram2018/Instagram2018/ResetPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResetPasswordViewController.swift
+//  Instagram2018
+//
+//  Created by CoderDream on 2018/12/5.
+//  Copyright © 2018 CoderDream. All rights reserved.
+//
+
+import UIKit
+
+class ResetPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Instagram2018/Instagram2018/SignInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignInViewController.swift
+//  Instagram2018
+//
+//  Created by CoderDream on 2018/12/5.
+//  Copyright © 2018 CoderDream. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
