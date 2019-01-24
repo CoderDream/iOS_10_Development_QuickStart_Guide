acc5a52bb1947fe38cbaf7d8d42f3038532ab785
许林 <xulin.wh@qq.com>
2019-01-24 17:12:22 +0800

    update

--- BookAssets/源代码/第7章/Instagram-finished/Instagram/SignInVC.swift
+++ BookAssets/源代码/第7章/Instagram-finished/Instagram/SignInVC.swift
@@ -7,55 +7,69 @@
 //
 
 import UIKit
-import AVOSCloud
+import LeanCloud
 
 class SignInVC: UIViewController {
-  
-  @IBOutlet weak var usernameTxt: UITextField!
-  @IBOutlet weak var passwordTxt: UITextField!
-  
-  @IBOutlet weak var signInBtn: UIButton!
-  @IBOutlet weak var signUpBtn: UIButton!
-  @IBOutlet weak var forgotBtn: UIButton!
-  
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
     
-    // Do any additional setup after loading the view.
-  }
-  
-  // 点击登陆按钮
-  @IBAction func signInBtn_click(_ sender: UIButton) {
-    print("登陆按钮被点击")
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
     
-    // 隐藏键盘
-    self.view.endEditing(true)
+    @IBOutlet weak var signInBtn: UIButton!
+    @IBOutlet weak var signUpBtn: UIButton!
+    @IBOutlet weak var forgotBtn: UIButton!
     
-    if usernameTxt.text!.isEmpty || passwordTxt.text!.isEmpty {
-      let alert = UIAlertController(title: "请注意", message: "请填写好所有的字段", preferredStyle: .alert)
-      let ok = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-      alert.addAction(ok)
-      self.present(alert, animated: true, completion: nil)
-    }
     
-    // 实现用户登录功能
-    AVUser.logInWithUsername(inBackground: usernameTxt.text!, password: passwordTxt.text!) { (user:AVUser?, error:NSError?) in
-      if error == nil {
-        // 记住用户
-        UserDefaults.standard().set(user!.username, forKey: "username")
-        UserDefaults.standard().synchronize()
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        // 调用AppDelegate类的login方法
-        let appDelegate: AppDelegate = UIApplication.shared().delegate as! AppDelegate
-        appDelegate.login()
-      }
+        // Do any additional setup after loading the view.
+        // 声明隐藏虚拟键盘的操作
+        let hideTap = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        hideTap.numberOfTapsRequired = 1
+        self.view.isUserInteractionEnabled = true
+        self.view.addGestureRecognizer(hideTap)
     }
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
+    
+    // 隐藏视图中的虚拟键盘
+    @objc
+    func hideKeyboard(recognizer: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    // 点击登陆按钮
+    @IBAction func signInBtn_click(_ sender: UIButton) {
+        print("登陆按钮被点击")
+        
+        // 隐藏键盘
+        self.view.endEditing(true)
+        
+        if usernameTxt.text!.isEmpty || passwordTxt.text!.isEmpty {
+            let alert = UIAlertController(title: "请注意", message: "请填写好所有的字段", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(ok)
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        // 实现用户登录功能
+        let result = LCUser.logIn(username: usernameTxt.text!, password: passwordTxt.text!)
+        
+        if result.isSuccess {
+            print("登录成功，用户名： \(usernameTxt.text!)")
+            // 记住登录的用户
+            UserDefaults.standard.set(usernameTxt.text!, forKey: "username")
+            UserDefaults.standard.synchronize()
+            
+            // 从 AppDelegate 类中调用 login 方法
+            let appDelegate: AppDelegate = UIApplication.shared.delegate as! AppDelegate
+            appDelegate.login()
+        } else {
+            print("登录失败")
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
 }
