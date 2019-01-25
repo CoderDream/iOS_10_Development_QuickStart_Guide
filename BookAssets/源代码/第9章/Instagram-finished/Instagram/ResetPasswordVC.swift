a4863c2753f45a5eda22cbdee9cbcd36241fbbeb
许林 <xulin.wh@qq.com>
2019-01-25 15:38:28 +0800

    升级第9章到Xcode10和Swift4.2，20190125

--- BookAssets/源代码/第9章/Instagram-finished/Instagram/ResetPasswordVC.swift
+++ BookAssets/源代码/第9章/Instagram-finished/Instagram/ResetPasswordVC.swift
@@ -7,81 +7,92 @@
 //
 
 import UIKit
-import AVOSCloud
+import LeanCloud
 
 class ResetPasswordVC: UIViewController {
   
-  @IBOutlet weak var emailTxt: UITextField!
-  @IBOutlet weak var resetBtn: UIButton!
-  @IBOutlet weak var cancelBtn: UIButton!
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    // UI元素布局
-    emailTxt.frame = CGRect(x: 10, y: 120, width: self.view.frame.width - 20, height: 30)
-    resetBtn.frame = CGRect(x: 20, y: emailTxt.frame.origin.y + 50, width: self.view.frame.width / 4, height: 30)
-    cancelBtn.frame = CGRect(x: self.view.frame.width / 4 * 3 - 20, y: resetBtn.frame.origin.y, width: self.view.frame.width / 4, height: 30)
-    
-    // 隐藏虚拟键盘的点击手势
-    let hideTap = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
-    hideTap.numberOfTapsRequired = 1
-    self.view.isUserInteractionEnabled = true
-    self.view.addGestureRecognizer(hideTap)
-  }
-  
-  func hideKeyboard(recognizer: UITapGestureRecognizer) {
-    self.view.endEditing(true)
-  }
-  
-  @IBAction func resetBtn_click(_ sender: AnyObject) {
-    // 隐藏键盘
-    self.view.endEditing(true)
-    
-    if emailTxt.text!.isEmpty {
-      let alert = UIAlertController(title: "请注意", message: "电子邮件不能为空", preferredStyle: .alert)
-      let ok = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-      alert.addAction(ok)
-      self.present(alert, animated: true, completion: nil)
-      
-      return
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var resetBtn: UIButton!
+    @IBOutlet weak var cancelBtn: UIButton!
+        
+    @IBAction func resetBtn_click(_ sender: AnyObject) {
+        // 隐藏键盘
+        self.view.endEditing(true)
+        
+        if emailTxt.text!.isEmpty {
+            let alert = UIAlertController(title: "请注意", message: "电子邮件不能为空", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(ok)
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        // 数据存储开发指南 · Swift
+        // https://leancloud.cn/docs/leanstorage_guide-swift.html#hash1214212664
+        let result:LCBooleanResult = LCUser.requestPasswordReset(email: emailTxt.text!)
+        
+        switch result {
+        case .success:
+            print("用户重置密码成功")
+            let alert = UIAlertController(title: "请注意", message: "重置密码连接已经发送到您的电子邮件！", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "OK", style: .default, handler: { (_) in
+                self.dismiss(animated: true, completion: nil)
+            })
+            alert.addAction(ok)
+            self.present(alert, animated: true, completion: nil)
+            break
+        case .failure(let error):
+            print("用户重置密码失败")
+            print(error.localizedDescription)
+            break
+        }
     }
     
-    AVUser.requestPasswordResetForEmail(inBackground: emailTxt.text) { (success:Bool, error:NSError?) in
-      if success {
-        let alert = UIAlertController(title: "请注意", message: "重置密码连接已经发送到您的电子邮件！", preferredStyle: .alert)
-        let ok = UIAlertAction(title: "OK", style: .default, handler: { (_) in
-          self.dismiss(animated: true, completion: nil)
-        })
-        alert.addAction(ok)
-        self.present(alert, animated: true, completion: nil)
-      }else {
-        print(error?.localizedDescription)
-      }
+    @IBAction func cancelBtn_click(_ sender: AnyObject) {
+        // 在单击取消按钮的时候隐藏键盘
+        self.view.endEditing(true)
+        
+        self.dismiss(animated: true, completion: nil)
     }
-  }
-  
-  @IBAction func cancelBtn_click(_ sender: AnyObject) {
     
-    self.view.endEditing(true)
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
-    self.dismiss(animated: true, completion: nil)
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-  
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
-  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // UI元素布局
+        emailTxt.frame = CGRect(x: 10, y: 120, width: self.view.frame.width - 20, height: 30)
+        resetBtn.frame = CGRect(x: 20, y: emailTxt.frame.origin.y + 50, width: self.view.frame.width / 4, height: 30)
+        cancelBtn.frame = CGRect(x: self.view.frame.width / 4 * 3 - 20, y: resetBtn.frame.origin.y, width: self.view.frame.width / 4, height: 30)
+        
+        // 隐藏虚拟键盘的点击手势
+        let hideTap = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        hideTap.numberOfTapsRequired = 1
+        self.view.isUserInteractionEnabled = true
+        self.view.addGestureRecognizer(hideTap)
+        
+        //设置背景图
+        let bg = UIImageView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+        
+        bg.image = UIImage(named: "bg.jpg")
+        bg.layer.zPosition = -1
+        self.view.addSubview(bg)
+    }
+    
+    @objc func hideKeyboard(recognizer: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
